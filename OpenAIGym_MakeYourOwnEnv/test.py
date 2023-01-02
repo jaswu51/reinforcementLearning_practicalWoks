30b210e165073c870be2f7a692a02a610ef0d43e
jaswu51 <506346013@qq.com>
2023-01-02 20:35:09 +0100

    push

--- OpenAIGym_MakeYourOwnEnv/test.py
+++ OpenAIGym_MakeYourOwnEnv/test.py
@@ -5,6 +5,7 @@ import gymnasium
 env = gym.make('gymexamples/GridWorld-v0')
 observation=env.reset(seed=None,options=None)
 print(observation)
+=======
 env = gymnasium.make('gymexamples/GridWorld-v0')
 print(env.metadata)
 >>>>>>> parent of 3913841 (push)
